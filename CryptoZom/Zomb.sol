fffec7c7b9045acaf2457d4f72834bd85bc213da
envs <olaonipekun2001@yahoo.com>
2022-07-05 15:20:17 +0200

    Adding a CryptoZom Sample Folder

---
+++ CryptoZom/Zomb.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract ZombieFactory {
+    // declare our event here
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint256 _dna) private {
+        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        // and fire it here
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
